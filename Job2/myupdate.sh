ebac29c23cdf8f9fc10089e7f75de0e803bbdf1e
abraham-ukachi <abraham.ukachi@laplateforme.io>
2022-10-03 06:24:37 +0200

    Update myupdate.sh & add .screenshots

--- Job2/myupdate.sh
+++ Job2/myupdate.sh
@@ -1,7 +1,15 @@
 #!/bin/zsh
 
-# macOS Mojave (10.14.6)
-# Homebrew (Gestionnaire de paquet)
-brew update
+# Job2
+# -------------------------------
+# Votre gestionnaire de paquets préféré a besoin d’être mis à jour de manière récurrente,
+# une tâche avec 2 lignes de commandes qui pourrait être simplifiée en une seule !
+# - Réalisez un script nommé myupdate.sh qui met à jour votre gestionnaire de
+# paquets#
+# -------------------------------
 
+
+# I'm using Homebrew (Gestionnaire de paquet) on a macOS Mojave (10.14.6)
+# The code below should update Homebrew - my package manager:
+brew update # <- Great! Another one-liner :|
 
